1db504edf97d35f58ef569085b999b604a48dd2c
Ryan Grover <ryangrover@Ryans-MacBook-Pro.local>
2023-07-24 16:54:35 -0700

    ui updates

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -23,9 +23,9 @@ export default function UIScreen() {
                     <Box
                         component="div"
                         sx={{
-                            width: '70%',
+                            width: '90%',
                             height: '50%',
-                            backgroundColor: 'white', // Replace this with your desired background color or image
+                            backgroundColor: 'rgba(255, 255, 255, 0.8)', // Replace this with your desired background color or image
                             borderRadius: '40px', // Adjust the value to control the roundness of the edges
                             border: '12px solid black', // Add a black solid border
                             display: 'flex',
@@ -47,10 +47,65 @@ export default function UIScreen() {
                         width: '100%',
                         height: '100%',
                         // backgroundColor: 'blue', // Replace this with your desired background color or image
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'flex-start',
                     }}
                 >
-                    {/* Content for the second box */}
-                    {/* <UIScreen /> */}
+
+                    <Box
+                        component="div"
+                        sx={{
+                            width: '90%',
+                            height: '50%',
+                            backgroundColor: 'rgba(255, 255, 255, 0.8)', // Replace this with your desired background color or image
+                            borderRadius: '40px', // Adjust the value to control the roundness of the edges
+                            border: '12px solid black', // Add a black solid border
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexDirection: 'column', // Set flexDirection to 'column'
+                            alignItems: 'center',
+                            margin: '40px', // Add padding to baby box 1
+                            marginTop: '-100px', // Move baby box 1 up by 20px
+                        }}
+                    >
+                        {/* baby text 1 */}
+                        <Typography sx={{ color: "black", fontSize: 64, textAlign: 'center' }}>
+                            Oppenheimer
+                        </Typography>
+                        {/* baby text 2 */}
+                        <Typography sx={{ color: "black", fontSize: 64, textAlign: 'center' }}>
+                            Votes: 120
+                        </Typography>
+                    </Box>
+
+
+                    <Box
+                        component="div"
+                        sx={{
+                            width: '90%',
+                            height: '50%',
+                            backgroundColor: 'rgba(255, 255, 255, 0.8)', // Replace this with your desired background color or image
+                            borderRadius: '40px', // Adjust the value to control the roundness of the edges
+                            border: '12px solid black', // Add a black solid border
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexDirection: 'column', // Set flexDirection to 'column'
+                            alignItems: 'center',
+                            margin: '40px', // Add padding to baby box 1
+                            marginTop: '-100px', // Move baby box 1 up by 20px
+                        }}
+                    >
+
+                        <Typography sx={{ color: "black", fontSize: 64, textAlign: 'center' }}>
+                            Barbie
+                        </Typography>
+
+                        <Typography sx={{ color: "black", fontSize: 64, textAlign: 'center' }}>
+                            Votes: 765
+                        </Typography>
+                    </Box>
+
                 </Box>
             </Grid>
             <Grid item xs={12}>
@@ -60,7 +115,7 @@ export default function UIScreen() {
                         position: 'relative',
                         width: '100%',
                         height: '100%',
-                        // backgroundColor: 'green', // Replace this with your desired background color or image
+                        backgroundColor: 'green', // Replace this with your desired background color or image
                     }}
                 >
                     {/* Content for the third box */}
